W427697/Discord-

Add some unit test

code/lib/core-server/src/utils/server-address.test.ts
@@ -2,6 +2,7 @@ import { describe, it, expect, vi } from 'vitest';
 import detectPort from 'detect-port';
 import { getServerAddresses, getServerPort, getServerChannelUrl } from './server-address';
 
+vi.mock('os');
 vi.mock('detect-port');
 vi.mock('@storybook/node-logger');
 

xeho91/storybook

code/lib/core-server/src/utils/server-address.test.ts
@@ -1,6 +1,7 @@
 import detectPort from 'detect-port';
 import { getServerAddresses, getServerPort, getServerChannelUrl } from './server-address';
 
+jest.mock('os');
 jest.mock('detect-port');
 jest.mock('@storybook/node-logger');
 