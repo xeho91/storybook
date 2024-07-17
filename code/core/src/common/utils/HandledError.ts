cf88938cd29c49713dda666f57733f8b3b6d5533
Norbert de Langen <ndelangen@me.com>
2024-07-17 10:22:24 +0200

    debugging

--- code/core/src/common/utils/HandledError.ts
+++ code/core/src/common/utils/HandledError.ts
@@ -2,6 +2,7 @@ export class HandledError extends Error {
   public handled = true;
 
   constructor(error: unknown) {
+    console.log({ error });
     super(String(error));
 
     if (typeof error !== 'string') this.cause = error;
