20771166737aa6fbbae361dd0dd98954e1694fdc
Mateusz Kadlubowski <xeho91@pm.me>
2024-07-20 13:50:44 +0800

    Simplify `merge` snippet - bye `lodash/merge` & `lodash/isEqual`

--- code/core/src/manager-api/lib/merge.ts
+++ code/core/src/manager-api/lib/merge.ts
@@ -1,23 +1,12 @@
-import mergeWith from 'lodash/mergeWith.js';
-import isEqual from 'lodash/isEqual.js';
+import { deepmerge } from 'deepmerge-ts';
 
 import { logger } from '@storybook/core/client-logger';
 
-export default <TObj = any>(a: TObj, b: Partial<TObj>) =>
-  mergeWith({}, a, b, (objValue: TObj, srcValue: Partial<TObj>) => {
-    if (Array.isArray(srcValue) && Array.isArray(objValue)) {
-      srcValue.forEach((s) => {
-        const existing = objValue.find((o) => o === s || isEqual(o, s));
-        if (!existing) {
-          objValue.push(s);
-        }
-      });
+export default <TObj = any>(a: TObj, b: Partial<TObj>) => {
+  if (Array.isArray(a) && !Array.isArray(b)) {
+    logger.log(['the types mismatch, picking', a]);
+    return a;
+  }
 
-      return objValue;
-    }
-    if (Array.isArray(objValue)) {
-      logger.log(['the types mismatch, picking', objValue]);
-      return objValue;
-    }
-    return undefined;
-  });
+  return deepmerge(a, b);
+};
